c896df2584b021d25d0d2415874499c1d2a0ae76
Tim Likarish <likarish@gmail.com>
2024-07-17 17:49:37 -0400

    [Ch2] Exercises
    Think this is exercise 2-1 to 2-4.

--- giraffe/browser.py
+++ giraffe/browser.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
+import math
 import tkinter
+from tkinter import BOTH
 from typing import List
 
 from giraffe.net import URL
@@ -10,9 +12,14 @@ This code is based on Chapter 2 of
 [Web Browser Engineering](https://browser.engineering/http.html).
 """
 
+DO_EXPAND = 1
 WIDTH, HEIGHT = 800, 600
 HSTEP, VSTEP = 13, 18
 SCROLL_STEP = 100
+SCROLL_MULTIPLIER = -20
+SCROLLBAR_WIDTH = 12
+SCROLLBAR_PAD = 4
+SCROLLBAR_COLOR = "cornflower blue"
 
 
 @dataclass
@@ -22,40 +29,85 @@ class DisplayUnit:
     c: str
 
 
+class FakeEvent:
+    delta: int
+
+
 class Browser(object):
     def __init__(self):
         self.window = tkinter.Tk()
-        self.canvas = tkinter.Canvas(self.window, width=WIDTH, height=HEIGHT)
-        self.canvas.pack()
+        self.width = WIDTH
+        self.height = HEIGHT
+        self.canvas = tkinter.Canvas(self.window, width=self.width, height=self.height)
+        self.canvas.pack(fill=BOTH, expand=DO_EXPAND)
         self.scroll = 0
         self.display_list = []
+        self.text = ""
         self.window.bind(sequence="<Down>", func=self.scrolldown)
         self.window.bind(sequence="<Up>", func=self.scrollup)
+        self.window.bind(sequence="<MouseWheel>", func=self.scrolldelta)
+        self.window.bind(sequence="<Configure>", func=self.configure)
 
     def load(self, url: URL):
         body = url.request()
-        text = lex(body, url.is_viewsource)
+        self.text = lex(body, url.is_viewsource)
         # display_list is standard browser/gui (?) terminology
-        self.display_list = layout(text)
+        self.display_list = layout(self.text, self.width)
         self.draw()
 
     def draw(self):
         self.canvas.delete("all")
+        self._display_text()
+        self._display_scrollbar()
+
+    def _display_text(self):
         for du in self.display_list:
             x, y, c = (du.cursor_x, du.cursor_y, du.c)
             if y + VSTEP < self.scroll:
                 continue
-            if y > self.scroll + HEIGHT:
+            if y > self.scroll + self.height:
                 continue
-
             self.canvas.create_text(x, y - self.scroll, text=c)
 
+    def _display_scrollbar(self):
+        first_y = self.display_list[0].cursor_y
+        last_y = self.display_list[-1].cursor_y
+        if first_y < self.scroll or last_y > self.scroll + self.height:
+            total_screens = math.ceil(last_y / self.height)
+            scrollbar_len = self.height / total_screens
+            scroll_perc = self.scroll / last_y
+            x1 = self.width - SCROLLBAR_WIDTH
+            y1 = scroll_perc * self.height + SCROLLBAR_PAD
+            x2 = self.width - SCROLLBAR_PAD
+            y2 = scroll_perc * self.height + scrollbar_len - SCROLLBAR_PAD
+            self.canvas.create_rectangle(x1, y1, x2, y2, fill=SCROLLBAR_COLOR, stipple='gray25')
+
+    def configure(self, e):
+        needs_draw = False
+        if self.width != e.width:
+            self.width = e.width
+            self.display_list = layout(self.text, self.width - SCROLLBAR_WIDTH)
+            needs_draw = True
+        if self.height != e.height:
+            self.height = e.height
+
+        if needs_draw:
+            self.draw()
+
     def scrolldown(self, _e):
-        self.scroll += SCROLL_STEP
-        self.draw()
+        self._handle_scroll(SCROLL_STEP)
 
     def scrollup(self, _e):
-        self.scroll -= SCROLL_STEP
+        self._handle_scroll(-SCROLL_STEP)
+
+    def scrolldelta(self, e):
+        delta = e.delta * SCROLL_MULTIPLIER
+        self._handle_scroll(delta)
+
+    def _handle_scroll(self, delta):
+        maxline = self.display_list[-1].cursor_y - self.height
+        # clamp scroll such that scroll doesn't go beyond the body
+        self.scroll = max(0, min(self.scroll + delta, maxline))
         self.draw()
 
 
@@ -88,13 +140,23 @@ def lex(body: str, is_viewsource=False) -> str:
     return result
 
 
-def layout(text) -> List[DisplayUnit]:
+def layout(text, width) -> List[DisplayUnit]:
     display_list = []
     cursor_x, cursor_y = HSTEP, VSTEP
+    prev_c = ""
     for c in text:
-        display_list.append(DisplayUnit(cursor_x, cursor_y, c))
-        cursor_x += HSTEP
-        if cursor_x >= WIDTH - HSTEP:
-            cursor_y += VSTEP
+        if c == "\n":
+            # XXX: skip over repeating new lines characters
+            if prev_c == "\n":
+                continue
+            cursor_y += VSTEP * 2
             cursor_x = HSTEP
+        else:
+            display_list.append(DisplayUnit(cursor_x, cursor_y, c))
+            cursor_x += HSTEP
+            if cursor_x >= width - HSTEP:
+                cursor_y += VSTEP
+                cursor_x = HSTEP
+
+        prev_c = c
     return display_list
--- giraffe/test_browser.py
+++ giraffe/test_browser.py
@@ -1,4 +1,4 @@
-from giraffe.browser import DisplayUnit, layout
+from giraffe.browser import WIDTH, DisplayUnit, HSTEP, VSTEP, layout
 
 LOREM_IPSUM = """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
 et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
@@ -7,19 +7,30 @@ Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deseru
 
 
 def test_layout():
-    display_list = layout("hi mom")
+    display_list = layout("hi mom", WIDTH)
     assert [
-        DisplayUnit(13 * 1, 18, "h"),
-        DisplayUnit(13 * 2, 18, "i"),
-        DisplayUnit(13 * 3, 18, " "),
-        DisplayUnit(13 * 4, 18, "m"),
-        DisplayUnit(13 * 5, 18, "o"),
-        DisplayUnit(13 * 6, 18, "m"),
+        DisplayUnit(HSTEP * 1, VSTEP, "h"),
+        DisplayUnit(HSTEP * 2, VSTEP, "i"),
+        DisplayUnit(HSTEP * 3, VSTEP, " "),
+        DisplayUnit(HSTEP * 4, VSTEP, "m"),
+        DisplayUnit(HSTEP * 5, VSTEP, "o"),
+        DisplayUnit(HSTEP * 6, VSTEP, "m"),
     ] == display_list
 
 
 def test_layout_wraps():
-    display_list = layout(LOREM_IPSUM)
-    assert display_list[0] == DisplayUnit(13, 18, "L")
-    assert display_list[-1] == DisplayUnit(364, 144, ".")
+    display_list = layout(LOREM_IPSUM, WIDTH)
+    assert display_list[0] == DisplayUnit(HSTEP, VSTEP, "L")
+    assert display_list[-1] == DisplayUnit(650, 216, ".")
 
+
+def test_layout_newlines():
+    display_list = layout("hello\nworld", WIDTH)
+    assert display_list[4] == DisplayUnit(HSTEP * 5, VSTEP, "o")
+    assert display_list[5] == DisplayUnit(HSTEP * 1, VSTEP * 3, "w")
+
+# XXX: re-visit this with more examples?
+def test_layout_skip_consecutive():
+    display_list = layout("hello\n\nworld", WIDTH)
+    assert display_list[4] == DisplayUnit(HSTEP * 5, VSTEP, "o")
+    assert display_list[5] == DisplayUnit(HSTEP * 1, VSTEP * 3, "w")
